f528a0bcf08e758ed3e1392006cc826f3d27e159
Noel Kirkland <noelkirkland@gmail.com>
2020-08-03 14:42:54 -0700

    finalize todolist
    Co-authored-by: Erich Richter <erich.richter@gmail.com>
    dquote> Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -36,11 +36,11 @@ namespace ToDoList.Controllers
 
     public ActionResult Details(int id)
     {
-      Category thisCategory = _db.Categories
-          .Include(category => category.Items)
-          .ThenInclude(join => join.Item)
-          .FirstOrDefault(category => category.CategoryId == id);
-      return View(thisCategory);
+      Category thisCategory = _db.Categories // all content in our Categories table
+          .Include(category => category.Items) // this is the "library punch card" to give you directions to find the specific associated items
+          .ThenInclude(join => join.Item) // using the "punch card" directions, here we get the actual items
+          .FirstOrDefault(category => category.CategoryId == id); // this finds the exact category we are looking for (based on the category id we are passing into the route) and returning that category and it's corresponding items
+      return View(thisCategory); // this is passing that category into the details view where we can narrow it down further e.g. Category.Item.Description
     }
 
     public ActionResult Edit(int id)
